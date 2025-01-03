e2aee32a451cb95e4645172f0d37b56908e84d7c
MOHAN S <mk3459347@gmail.com>
2025-01-03 11:09:34 +0530

    python
    python basics operator

---
+++ operators.py
@@ -0,0 +1,46 @@
+num1 =15
+num2 =25
+# addition
+sum=num1+num2
+# subtraction
+sub=num1-num2
+# multiplication
+diff=num1*num2
+# division
+div=num1/num2
+# total results
+print(num1+num2,num1-num2,num1*num2,num1/num2)
+
+# new one
+num1 =15
+num2 =25
+# addition
+sum=num1+num2
+print("the addition of two numbers is ",sum)
+# subtraction
+sub=num1-num2
+print("the subtraction of two numbers is ",sub)
+# multiplication
+diff=num1*num2
+print("the multiple of two numbers is ",diff)
+# division
+div=num1/num2
+print("the division of two numbers is ",div)
+  
+
+
+  #new one
+num1 =15
+num2 =25
+sum=num1+num2
+print("the addition of two numbers is ",sum)
+sub=num1-num2
+print("the subtraction of two numbers is ",sub)
+diff=num1*num2
+print("the multiple of two numbers is ",diff)
+div=num1/num2
+print("the division of two numbers is ",div)
+less=num1<num2
+print("the lesser of two numbers is",less)
+greater=num1>num2
+print("the greater of two numbers is",greater)
